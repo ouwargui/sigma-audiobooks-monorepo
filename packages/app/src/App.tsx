71a2c756dd104e9fe41198fdabf983db6e24952e
ouwargui <guikemon2@gmail.com>
2023-04-22 05:06:54 -0300

    feat: setup basic audio player on app

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StatusBar} from 'expo-status-bar';
-import {Text, View} from 'react-native';
+import {Button, Text, View} from 'react-native';
 import {httpBatchLink} from '@trpc/react-query';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {trpc} from './utils/trpc';
+import {Audio} from 'expo-av';
 
 const trpcClient = trpc.createClient({
   links: [
@@ -19,8 +20,7 @@ export default function App() {
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
         <View className="flex-1 bg-white items-center justify-center">
-          <Text>Open up App.tsx to start working on your app!</Text>
-          <Teste />
+          <AudioPlayer />
           <StatusBar style="auto" />
         </View>
       </QueryClientProvider>
@@ -28,16 +28,55 @@ export default function App() {
   );
 }
 
-function Teste() {
-  const userListQuery = trpc.books.getAll.useQuery();
-  const userByIdQuery = trpc.reviews.getById.useQuery(1);
+const uri =
+  'https://d19manr6rwgxys.cloudfront.net/books/alice/aliceinwonderland_00_carroll_64kb.mp3';
+const uri2 =
+  'https://d19manr6rwgxys.cloudfront.net/books/alice/aliceinwonderland_01_carroll_64kb.mp3';
+
+function AudioPlayer() {
+  const [audio, setAudio] = useState<Audio.Sound>();
+  const book = trpc.books.getById.useQuery(1);
+
+  const playSound = async () => {
+    await Audio.setAudioModeAsync({
+      playsInSilentModeIOS: true,
+    });
+    console.log('Loading Sound');
+    const {sound, status} = await Audio.Sound.createAsync({uri});
+    setAudio(sound);
+
+    if (status.isLoaded) {
+      setAudio(sound);
+      await sound.playAsync();
+    }
+  };
+
+  const playNext = async () => {
+    await audio?.stopAsync();
+    await audio?.unloadAsync();
+    const {sound, status} = await Audio.Sound.createAsync({uri: uri2});
+
+    if (status.isLoaded) {
+      setAudio(sound);
+      await sound.playAsync();
+    }
+  };
 
   return (
-    <View>
-      {userListQuery.data?.map((user) => (
-        <Text key={user.id}>{user.author}</Text>
-      ))}
-      <Text>{userByIdQuery.data?.title}</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="bg-red-200">{book.data?.title}</Text>
+      <Button
+        title="Play"
+        onPress={(e) => {
+          void playSound();
+        }}
+      />
+      <Button
+        title="Next"
+        onPress={(e) => {
+          void playNext();
+        }}
+      />
     </View>
   );
 }
