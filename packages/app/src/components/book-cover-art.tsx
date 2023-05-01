0884a01efc5d50abe6710b7eb1d9694aa4313193
ouwargui <guikemon2@gmail.com>
2023-05-01 01:11:01 -0300

    feat: create placeholder for book cover art

--- packages/app/src/components/book-cover-art.tsx
+++ packages/app/src/components/book-cover-art.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Image} from 'react-native';
 
 type Props = {
@@ -6,9 +6,16 @@ type Props = {
 };
 
 const BookCoverArt: React.FC<Props> = ({coverArtUrl}) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   return (
     <View className="w-full aspect-[9/16] rounded-xl overflow-hidden">
-      <Image className="w-full h-full" source={{uri: coverArtUrl}} />
+      <Image
+        className="w-full h-full"
+        source={{uri: coverArtUrl}}
+        onLoad={() => setImageLoaded(true)}
+      />
+      {!imageLoaded && <View className="absolute w-full h-full bg-gray-100" />}
     </View>
   );
 };
