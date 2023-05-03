f6e89dfbf36692e6ddb1ac03fb36466450a38967
ouwargui <guikemon2@gmail.com>
2023-05-03 00:27:45 -0300

    feat: setup universal links on ios

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -3,7 +3,12 @@ import {trpcExpressMiddleware} from '@sigma-audiobooks/shared';
 
 const app = express();
 app.use('/api/trpc', trpcExpressMiddleware);
-app.use('/api', (_, res) => res.status(200).send('service is running'));
+app.get('/api', (_, res) => res.status(200).send('service is running'));
+app.get('/.well-known/apple-app-site-association', (_, res) => {
+  return res.status(200).sendFile('apple-app-site-association', {
+    root: './public',
+  });
+});
 
 app.listen(process.env.PORT ?? 3000, () =>
   console.log(`app listening on port ${process.env.PORT ?? 3000}`),
