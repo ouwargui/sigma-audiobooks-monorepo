eaac79a533bccf73a2bf489de2de5462279be691
ouwargui <guikemon2@gmail.com>
2023-05-02 23:41:18 -0300

    feat: create trpc procedure to get recently added books with pagination

--- packages/shared/trpc/routes/books.ts
+++ packages/shared/trpc/routes/books.ts
@@ -33,10 +33,39 @@ export function setupBookRouter(
         where: {
           title: {
             contains: input,
+            mode: 'insensitive',
           },
         },
         orderBy: {title: 'asc'},
       });
     }),
+    getRecentlyAdded: pProcedure
+      .input(
+        z.object({
+          limit: z.number().min(1).max(50).nullish(),
+          cursor: z.number().nullish(),
+        }),
+      )
+      .query(async ({input, ctx}) => {
+        const limit = input.limit ?? 50;
+        const cursor = input.cursor;
+
+        const books = await ctx.prisma.book.findMany({
+          take: limit + 1,
+          cursor: cursor ? {id: cursor} : undefined,
+          orderBy: {id: 'desc'},
+        });
+
+        let nextCursor: typeof cursor = undefined;
+        if (books.length > limit) {
+          const nextBook = books.pop();
+          nextCursor = nextBook?.id;
+        }
+
+        return {
+          books,
+          nextCursor,
+        };
+      }),
   });
 }
