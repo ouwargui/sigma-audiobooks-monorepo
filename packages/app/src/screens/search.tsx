e93ae6ae45164f8772a9dea775aff2462bb0f4b7
ouwargui <guikemon2@gmail.com>
2023-05-17 22:44:16 -0300

    feat: create scrollRef on each screen and send to wrapper

--- packages/app/src/screens/search.tsx
+++ packages/app/src/screens/search.tsx
@@ -1,4 +1,4 @@
-import React, {PropsWithChildren, useEffect, useState} from 'react';
+import React, {PropsWithChildren, useEffect, useRef, useState} from 'react';
 import {TextInput, Text, View, NativeScrollEvent} from 'react-native';
 import Wrapper from '../components/wrapper';
 import {Ionicons} from '@expo/vector-icons';
@@ -76,6 +76,7 @@ const SearchWrapper: React.FC<SearchWrapperProps> = ({
   children,
   isDarkMode,
 }) => {
+  const scrollRef = useRef<Animated.FlatList<Book>>(null);
   const scrollYOffset = useSharedValue(0);
   const keyboard = useAnimatedKeyboard();
 
@@ -148,7 +149,8 @@ const SearchWrapper: React.FC<SearchWrapperProps> = ({
   };
 
   return (
-    <Wrapper<Book>
+    <Wrapper
+      scrollRef={scrollRef}
       title="Search"
       onScroll={onScroll}
       flatList
