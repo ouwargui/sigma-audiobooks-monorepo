83bfb3b1ab42a5630b22b64984cdcb7cffa130d6
ouwargui <guikemon2@gmail.com>
2023-05-18 23:29:29 -0300

    feat: remove updater screen and move updates to background

--- packages/app/src/providers/update-provider.tsx
+++ packages/app/src/providers/update-provider.tsx
@@ -1,11 +1,9 @@
-import React, {PropsWithChildren, useEffect, useState} from 'react';
+import React, {PropsWithChildren, useState} from 'react';
 import * as ExpoUpdates from 'expo-updates';
-import {Alert} from 'react-native';
 
 type UpdateContextType = {
-  finishedUpdating: boolean;
-  isDeepLinking: boolean;
-  setIsDeepLinking: React.Dispatch<React.SetStateAction<boolean>>;
+  finishedCheckingForUpdate: boolean;
+  updateAvailable: boolean;
 };
 
 export const UpdateContext = React.createContext<UpdateContextType>(
@@ -13,49 +11,36 @@ export const UpdateContext = React.createContext<UpdateContextType>(
 );
 
 const UpdateProvider: React.FC<PropsWithChildren> = ({children}) => {
-  const [finishedUpdating, setFinishedUpdating] = useState(false);
-  const [isDeepLinking, setIsDeepLinking] = useState(false);
+  const [finishedCheckingForUpdate, setFinishedCheckingForUpdate] =
+    useState(false);
+  const [updateAvailable, setUpdateAvailable] = useState(false);
 
-  const fetchUpdates = async () => {
-    try {
-      if (__DEV__) {
-        return setFinishedUpdating(true);
-      }
+  const updatesListener = (event: ExpoUpdates.UpdateEvent) => {
+    if (event.type === ExpoUpdates.UpdateEventType.NO_UPDATE_AVAILABLE) {
+      setFinishedCheckingForUpdate(true);
+      setUpdateAvailable(false);
+      return;
+    }
 
-      const update = await ExpoUpdates.checkForUpdateAsync();
+    if (event.type === ExpoUpdates.UpdateEventType.UPDATE_AVAILABLE) {
+      setFinishedCheckingForUpdate(true);
+      setUpdateAvailable(true);
+      return;
+    }
 
-      if (update.isAvailable) {
-        const fetchedUpdate = await ExpoUpdates.fetchUpdateAsync();
-        if (fetchedUpdate.isNew) {
-          await ExpoUpdates.reloadAsync();
-        }
-      }
-      setFinishedUpdating(true);
-    } catch (error) {
-      const err = error as Error;
-      Alert.alert(
-        'Error',
-        `Failed to update the app. Please try again later.\nDetails: ${err.message}`,
-        [
-          {
-            text: 'OK',
-            onPress: () => {
-              setFinishedUpdating(true);
-            },
-          },
-        ],
-      );
+    if (event.type === ExpoUpdates.UpdateEventType.ERROR) {
+      setFinishedCheckingForUpdate(true);
+      setUpdateAvailable(false);
+      console.error(event.message);
+      return;
     }
   };
 
-  useEffect(() => {
-    void fetchUpdates();
-  }, []);
+  ExpoUpdates.useUpdateEvents(updatesListener);
 
   const returnValues: UpdateContextType = {
-    finishedUpdating,
-    isDeepLinking,
-    setIsDeepLinking,
+    finishedCheckingForUpdate,
+    updateAvailable,
   };
 
   return (
