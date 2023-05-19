4711df2e1352985c2b13c5121b81917ae2a486e0
ouwargui <guikemon2@gmail.com>
2023-05-18 23:37:02 -0300

    chore: add background color to screens that weren't developed yet

--- packages/app/src/screens/profile.tsx
+++ packages/app/src/screens/profile.tsx
@@ -3,7 +3,7 @@ import {View, Text} from 'react-native';
 
 const Profile: React.FC = () => {
   return (
-    <View>
+    <View className="flex-1 bg-white dark:bg-zinc-900">
       <Text>Profile</Text>
     </View>
   );
