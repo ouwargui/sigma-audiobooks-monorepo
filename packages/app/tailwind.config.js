e89f67cc50378adfb2127e0cb29e5b0611f7481e
ouwargui <guikemon2@gmail.com>
2023-05-20 21:23:02 -0300

    feat: extend tailwind color names

--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
@@ -14,6 +16,17 @@ module.exports = {
         mediumItalic: 'RedHatMono_500Medium_Italic',
         semiItalic: 'RedHatMono_600SemiBold_Italic',
         boldItalic: 'RedHatMono_700Bold_Italic',
+      },
+      backgroundColor: {
+        light: colors.white,
+        dark: colors.zinc[900],
+      },
+      textColor: {
+        'primary-light': colors.zinc[800],
+        'primary-dark': colors.zinc[100],
+        'secondary-light': colors.zinc[500],
+        'secondary-dark': colors.zinc[400],
+        'label': colors.zinc[300],
       },
     },
   },
